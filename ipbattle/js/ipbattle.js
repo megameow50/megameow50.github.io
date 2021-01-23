980c1323b24990f4e53f6e11c5c8508b43b51bd7
Simon <megameow50@gmail.com>
2021-01-23 16:51:20 -0500

    added ipbattle

---
+++ ipbattle/js/ipbattle.js
@@ -0,0 +1,67 @@
+"use strict"
+"use strict"
+class Fighter {
+	constructor(n, c) {
+		this.name = n;	//Name
+		this.cat = c;	//Category
+	}
+
+toString() {
+return this.name;
+
+}
+
+}
+
+var bigList = [];
+bigList.push(new Fighter("Darth Vader", "Star Wars"));
+bigList.push(new Fighter("Darth Maul", "Star Wars"));
+bigList.push(new Fighter("Boba Fett", "Star Wars"));
+bigList.push(new Fighter("Din Djarin", "Star Wars"));
+bigList.push(new Fighter("Mace Windu", "Star Wars"));
+bigList.push(new Fighter("Qui Gon Jin", "Star Wars"));
+bigList.push(new Fighter("Luke Skywalker", "Star Wars"));
+bigList.push(new Fighter("Kylo Ren", "Star Wars"));
+bigList.push(new Fighter("Moff Gideon", "Star Wars"));
+bigList.push(new Fighter("General Grievous", "Star Wars"));
+bigList.push(new Fighter("Asokah Tano", "Star Wars"));
+bigList.push(new Fighter("Obi Wan Kenobi", "Star Wars"));
+bigList.push(new Fighter("Grogu", "Star Wars"));
+bigList.push(new Fighter("Yoda", "Star Wars"));
+bigList.push(new Fighter("Count Dooku", "Star Wars"));
+bigList.push(new Fighter("Supreme Leader Snoke", "Star Wars"));
+bigList.push(new Fighter("Emperor Palpatine", "Star Wars"));
+
+bigList.push(new Fighter("Dumbledore", "Harry Potter"));
+bigList.push(new Fighter("Grindelwald", "Harry Potter"));
+bigList.push(new Fighter("Voldemort", "Harry Potter"));
+bigList.push(new Fighter("Harry Potter", "Harry Potter"));
+bigList.push(new Fighter("Sirius Black", "Harry Potter"));
+
+bigList.push(new Fighter("Zuko", "Avatar"));
+bigList.push(new Fighter("Aang", "Avatar"));
+bigList.push(new Fighter("Azula", "Avatar"));
+bigList.push(new Fighter("Iroh", "Avatar"));
+bigList.push(new Fighter("Amon", "Avatar"));
+bigList.push(new Fighter("Zaheer", "Avatar"));
+bigList.push(new Fighter("Unavaatu", "Avatar"));
+
+bigList.push(new Fighter("Gandalf", "Lord Of The Rings"));
+bigList.push(new Fighter("Sauron", "Lord Of The Rings"));
+bigList.push(new Fighter("Saruman", "Lord Of The Rings"));
+bigList.push(new Fighter("Smaug", "Lord Of The Rings"));
+
+bigList.push(new Fighter("Captain America", "MCU"));
+bigList.push(new Fighter("Captain Marvel", "MCU"));
+bigList.push(new Fighter("Loki", "MCU"));
+bigList.push(new Fighter("Iron Man", "MCU"));
+bigList.push(new Fighter("Thor", "MCU"));
+bigList.push(new Fighter("Iron Monger", "MCU"));
+bigList.push(new Fighter("Thanos", "MCU"));
+bigList.push(new Fighter("Black Widow", "MCU"));
+bigList.push(new Fighter("Ebony Maw", "MCU"));
+bigList.push(new Fighter("Corvus Glave", "MCU"));
+bigList.push(new Fighter("Cull Obsidian", "MCU"));
+bigList.push(new Fighter("Proxima Midnight", "MCU"));
+bigList.push(new Fighter("Falcon", "MCU"));
+bigList.push(new Fighter("The Winter Soldier", "MCU"));
